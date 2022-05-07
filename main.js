c27d0afb611f09db7dda16a74a3d2dac4877d4c2
sarthaksavvy <sarthak@bitfumes.com>
2022-05-07 11:06:57 +0530

    02. ElectronJS - activate application

--- main.js
+++ main.js
@@ -11,4 +11,12 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
